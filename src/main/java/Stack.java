9d7d0cbb7ebe909c1b4ba7c024fc98be25a6a08a
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-07 13:09:48 +0200

    Create spaceLeft method

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -7,4 +7,12 @@ public class Stack<T> {
         this.maxSize = maxSize;
         this.arr = new Object[maxSize];
     }
+
+    public int spaceLeft() {
+        int counter = 0;
+        for (int i = 0; i < arr.length; i ++) {
+            if (arr[i] != null) counter++;
+        }
+        return maxSize - counter;
+    }
 }
