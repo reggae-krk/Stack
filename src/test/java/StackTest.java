6d1c7b233a7afbca2559b20789224b4b1d8b0017
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-07 14:00:13 +0200

    Create createStack method

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -4,6 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
 
+    private Stack<String> createStack() {
+        Stack<String> stack = new Stack<String>(String.class, 5);
+        return stack;
+    }
+
     @Test
     void testCreateStack() {
         Stack<String> newStack = new Stack<String>(String.class, 10);
